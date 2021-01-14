3b60a3cad836ca0d846dc5b2566dd8edc01eab94
GhostCCCatHenry <45286297+GhostCCCatHenry@users.noreply.github.com>
2021-01-14 19:50:08 +0800

    2021-1-14

--- static/Vue/main.js
+++ static/Vue/main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el:'#app',
     data:{
-        message:'hello vue!'
+        message:'hello vue!',
+        school:{
+            name:"alex",
+            id:123
+        },
+        address:["dwad","dawd","dwad"]
     }
-});
+});//挂载vue实例！
